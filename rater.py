0db9f347d0c13180ee1a90af96e3616498975806
daveo30 <devanshojha30@gmail.com>
2016-12-16 23:47:14 +0530

    Rates all files in particular folder

--- rater.py
+++ rater.py
@@ -1,11 +1,27 @@
 from mechanize import Browser
 import json
 import urllib2
-print "Enter movie name"
-movie = raw_input()
-movie= movie.replace(" ","+")
+import os
+
+
+from os import walk
+movies = []
+for (dirpath, dirnames, filenames) in walk("/home/dojha/Videos"):
+    movies.extend(dirnames)
+    break
+for movie in movies:
+    print movie
+    x=""
+    curmovie= movie.replace(" ","+")
+    for i in curmovie:
+        if i=="." or i=="(" or i==")" or i=="," or i==":" or i==";" or i=="[":
+            break
+        x+=i
 #req = urllib2.urlopen("http://www.omdbapi.com/?t=shawshank+redemption&y=&plot=short&r=json").read()
-req = urllib2.urlopen("http://www.omdbapi.com/?t="+movie+"&y=&plot=short&r=json").read()
-moviedetjson=json.loads(req)
-print(moviedetjson['Title'])
-print(moviedetjson['imdbRating'])
+    try:
+        req = urllib2.urlopen("http://www.omdbapi.com/?t="+x+"&y=&plot=short&r=json").read()
+        moviedetjson=json.loads(req)
+        print(moviedetjson['Title'])
+        print(moviedetjson['imdbRating'])
+    except:
+        print "not found"
